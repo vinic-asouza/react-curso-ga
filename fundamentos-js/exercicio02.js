5712704d09a279600e11b306d96795c40c36240e
Vinicius Souza <vinicius.souza@auaha.com.br>
2022-09-10 14:30:00 -0300

    exercicios

---
+++ fundamentos-js/exercicio02.js
@@ -0,0 +1,10 @@
+let newStr = '';
+
+function reverseAString(str) {
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]; 
+    }
+    console.log(newStr);
+}
+
+let resultado = reverseAString('Hello')
